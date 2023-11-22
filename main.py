0cc80d5ccd2e1bbd1d52372a84020982989f226a
vashenko_andrei <vashenko_andrei@mail.ru>
2023-11-22 14:48:26 +0500

    test

---
+++ main.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+if __name__ == '__main__':
+    print('PyCharm')
+    path_new_dir = Path("C:\\123")
+    #path_new_dir.mkdir()
+    print(path_new_dir.exists())
+    #ss = input("enter any word")
