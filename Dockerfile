d04b87e5a75c23d18d3b3dceedd6bcabf4c6b5cb
vashenko_andrei <vashenko_andrei@mail.ru>
2023-11-24 16:06:35 +0500

    test

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY . .
 
-VOLUME C://temp_os:./temp
+#VOLUME C://temp_os:/temp
+
+VOLUME C:\temp_os\os.txt:/temp
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -6,13 +6,11 @@ if __name__ == '__main__':
     #path_new_dir = Path("C://temp_os/os.txt")
     #path_new_dir = Path("./temp/py.txt")
     #path_new_dir = Path("./temp/os.txt")
-    path_new_dir1 = Path("./temp_os.txt")
-    path_new_dir2 = Path("./temp/temp_os.txt")
-    path_new_dir3 = Path("./temp/py.txt")
+    path_new_dir1 = Path("./temp/os.txt")
+    path_new_dir2 = Path("./temp/py.txt")
 
     #path_new_dir.mkdir()
     print(path_new_dir1.exists())
     print(path_new_dir2.exists())
-    print(path_new_dir3.exists())
 
     ss = input("enter any word")
