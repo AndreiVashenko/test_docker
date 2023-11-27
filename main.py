695eeac3aeed609bb4236f220b0f47cdfc622420
vashenko_andrei <vashenko_andrei@mail.ru>
2023-11-27 16:18:58 +0500

    test

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
+import time
 from pathlib import Path
 
+
 if __name__ == '__main__':
-    print('PyCharm')
-    #path_new_dir = Path(".\temp\os.txt")
-    #path_new_dir = Path("C://temp_os/os.txt")
-    #path_new_dir = Path("./temp/py.txt")
-    #path_new_dir = Path("./temp/os.txt")
-    path_new_dir1 = Path("./temp/os.txt")
-    path_new_dir2 = Path("./temp/py.txt")
 
-    #path_new_dir.mkdir()
-    print(path_new_dir1.exists())
-    print(path_new_dir2.exists())
 
-    ss = input("enter any word")
+    while time.sleep(120):
+        pass
