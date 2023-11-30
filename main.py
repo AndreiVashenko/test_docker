c639dfdb9902a4a5a7b8f9349613d44749642bf5
AndreyW <vashenko_andrei@mail.ru>
2023-11-30 19:56:11 +0500

    123

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pathlib import Path
+import time
 
 
 if __name__ == '__main__':
 
+    while time.sleep(600):
+       pass
+    #g = input("enter any word")
 
-    #while time.sleep(120):
-    #   pass
-    g = input("enter any word")
-
-    vol = Path("../var/lib/docker")
-    print(vol.exists())
+    #vol = Path("../var/lib/docker")
+    #print(vol.exists())
 
